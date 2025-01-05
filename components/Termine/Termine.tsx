41affdb175122880c4289ff2823e38f93e7d8e9f
Edgar Klepek <edgarklepek@gmail.com>
2025-01-05 21:23:24 +0100

    Add new route and page for termine

---
+++ components/Termine/Termine.tsx
@@ -0,0 +1,5 @@
+export const Termine = () => {
+    return <>
+        <h1>Termine</h1>
+    </>
+}
---
+++ pages/termine.tsx
@@ -0,0 +1,9 @@
+import {Termine} from '@/components/Termine/Termine';
+
+export default function DatesPage() {
+    return (
+        <>
+            <Termine />
+        </>
+    );
+}
