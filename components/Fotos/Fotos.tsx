f86135c55b4d8ebe9cca94e60125f4fb9fac1482
Edgar Klepek <edgarklepek@gmail.com>
2025-01-06 14:18:38 +0100

    Create fotos

--- components/Fotos/Fotos.tsx
+++ components/Fotos/Fotos.tsx
@@ -1,24 +1,28 @@
-import { Container, Grid, SimpleGrid, Skeleton } from '@mantine/core';
+import { Container, Grid, SimpleGrid } from '@mantine/core';
+import Image from 'next/image';
 
 export const Fotos = () => {
-  const PRIMARY_COL_HEIGHT = '300px';
-  const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - var(--mantine-spacing-md) / 2)`;
-
   return (
     <>
       <Container my="md">
         <h1>Fotos</h1>
         <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="md">
-          <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} />
           <Grid gutter="md">
-            <Grid.Col>
-              <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+            <Grid.Col span={12}>
+              <Image
+                  className='mr-4'
+                  src='/images/0125_01.JPG'
+                  height={400}
+                  width={600}
+                  alt='Shiba'
+              />
+              <Image
+                  className='mr-4'
+                  src='/images/0125_02.JPG'
+                  height={400}
+                  width={600}
+                  alt='Shiba'
+              />
             </Grid.Col>
           </Grid>
         </SimpleGrid>
