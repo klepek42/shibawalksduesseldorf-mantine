7e6781db36b987c9755c8e187cb591321297eeeb
Edgar Klepek <edgarklepek@gmail.com>
2025-01-06 14:19:11 +0100

    Refactoring

--- components/Termine/DateEntry.tsx
+++ components/Termine/DateEntry.tsx
@@ -1,34 +1,30 @@
-import {Card, Text} from '@mantine/core';
+import { Card, Text } from '@mantine/core';
 
 interface DateEntry {
-    title: string,
-    date: string,
-    city: string,
-    location: string,
-    parking: string,
-    maps: string
+  title: string;
+  date: string;
+  city: string;
+  location: string;
+  parking: string;
+  maps: string;
 }
 
 interface Props {
-    date: DateEntry
+  date: DateEntry;
 }
 
-export const DateEntry = ({date}:Props) => {
-    const today = new Date();
-    const entryDate = new Date('06.01.2025')
-    const dateExpired = entryDate < today
+export const DateEntry = ({ date }: Props) => {
+  const today = new Date();
+  const entryDate = new Date('06.01.2025');
+  const dateExpired = entryDate < today;
 
-    return <div>
-        <Card
-            shadow="sm"
-            padding="xl"
-            component="a"
-            radius="md"
-            withBorder
-        >
-            <Text fw={500} size="lg" mt="md" color={`${dateExpired ? 'grey' : 'black'}`}>
-                {date.title} am {date.date} in {date.city}
-            </Text>
-        </Card>
+  return (
+    <div style={{marginBottom: '1em'}}>
+      <Card padding="xl" component="a" radius="md" withBorder>
+        <Text fw={500} size="lg" mt="md" color={`${dateExpired ? 'grey' : 'black'}`}>
+          {date.title} am {date.date} in {date.city}
+        </Text>
+      </Card>
     </div>
-}
+  );
+};
